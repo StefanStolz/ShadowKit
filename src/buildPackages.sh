3dc3f059b8c250b5dae6c4ed5ff37bfb11e92895
Stefan Stolz <stefan.stolz@gmx.de>
2025-06-15 12:20:18 +0200

    MetaPackage

--- src/buildPackages.sh
+++ src/buildPackages.sh
@@ -3,3 +3,4 @@
 dotnet pack ShadowKit.IO/ShadowKit.IO.csproj
 dotnet pack ShadowKit.Text/ShadowKit.Text.csproj
 dotnet pack ShadowKit.Threading/ShadowKit.Threading.csproj
+dotnet pack ShadowKit/ShadowKit.csproj
