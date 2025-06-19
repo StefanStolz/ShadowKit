65435828ff26b7e4b40fb2dac01da5586a53b896
Stefan Stolz <stefan.stolz@gmx.de>
2025-06-19 12:51:45 +0200

    Add XML documentation for LineEndingKind, DetectLineEnding, and StringExtensions

--- src/ShadowKit.Text/DetectLineEnding.cs
+++ src/ShadowKit.Text/DetectLineEnding.cs
@@ -3,12 +3,37 @@ using System.Linq;
 
 namespace ShadowKit.Text
 {
+    /// <summary>
+    /// Represents the different types of line endings that may be present in a string.
+    /// </summary>
     public enum LineEndingKind
     {
+        /// <summary>
+        /// Represents an undefined or unrecognized type of line ending.
+        /// This value is used when the line ending format cannot be determined.
+        /// </summary>
         Unknown,
+
+        /// <summary>
+        /// Represents the Windows line ending format, specifically using a Carriage Return
+        /// followed by a Line Feed ('\r\n') as the delimiter between lines.
+        /// </summary>
         Windows,
+
+        /// <summary>
+        /// Represents the Unix line ending style, which uses a single line feed character ('\n') to terminate lines.
+        /// </summary>
         Unix,
+
+        /// <summary>
+        /// Represents the line ending style used in classic Mac OS, which uses a carriage return ('\r') character.
+        /// </summary>
         Mac,
+
+        /// <summary>
+        /// Represents a mixed line ending style where multiple types of line endings
+        /// (e.g., Windows, Unix, or Mac) are simultaneously present in a given input.
+        /// </summary>
         Mixed
     }
 
@@ -17,6 +42,17 @@ namespace ShadowKit.Text
     /// </summary>
     public sealed class DetectLineEnding
     {
+        /// <summary>
+        /// Detects the type of line endings used in the given text input.
+        /// </summary>
+        /// <param name="input">
+        /// A string containing the text whose line endings are to be detected.
+        /// </param>
+        /// <returns>
+        /// A value of the <see cref="LineEndingKind"/> enumeration representing the type of line endings
+        /// detected in the input. Returns <see cref="LineEndingKind.Unknown"/> if no line endings are detected,
+        /// or <see cref="LineEndingKind.Mixed"/> the line endings are not consistent.
+        /// </returns>
         public LineEndingKind DetectFromString(string input)
         {
             int lfCount = input.Count(c => c == '\n');
--- src/ShadowKit.Text/StringExtensions.cs
+++ src/ShadowKit.Text/StringExtensions.cs
@@ -5,6 +5,13 @@ namespace ShadowKit.Text;
 
 public static class StringExtensions
 {
+    /// <summary>
+    /// Splits the input string into its individual lines.
+    /// </summary>
+    /// <param name="input">The input string to be split into lines.</param>
+    /// <returns>
+    /// An enumerable collection of strings, where each string represents a line from the input.
+    /// </returns>
     public static IEnumerable<string> ToLines(this string input)
     {
         using var stringReader = new StringReader(input);
